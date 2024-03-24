f4a55aad89262a87b9f1dc06d2efc885ff26eb44
Natalie Thompson <nataliethompson2314@gmail.com>
2024-03-24 16:24:22 -0700

    finished

--- frontend/components/Todo.js
+++ frontend/components/Todo.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
 
 export default class Todo extends React.Component {
+
+  // handleToggle = () => {
+  //   const { todo, togglePurchased } = this.props;
+  //   togglePurchased(todo.id);
+  //   this.setState(prevState => ({
+  //     completed: !prevState.completed
+  //   }));
+  // };
+
   render() {
+    
     return (
-      <div>
-        Todo
+      <div onClick={this.props.onClickTodo(this.props.todo.id)}>
+        <span>
+          {this.props.todo.name}
+          {this.props.todo.completed ? " ✔️" : ""}
+        </span>
       </div>
-    )
+    );
   }
 }
